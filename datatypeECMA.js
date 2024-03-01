d88dd8eb40db05f4b5262dd9139edae61f80093f
KrishnaNarde <krishnanarde2001@gmail.com>
2024-03-01 19:09:32 +0530

    some information about Datatypes

---
+++ datatypeECMA.js
@@ -0,0 +1,21 @@
+//// all about datatype and ECMA scripts///
+"use strict"; // treat all JS code as newer version {defined by new standards}
+
+//alert (3+3) // not able to get o/p --> beacise we are using NodeJs and not browser
+ //Note:-  we have to maintain code readibility in javascript
+
+
+ // datatypes in js
+ //number==> 2to power 53
+ //bigint
+ //string=>=""
+ //boolean=>true/false
+ // null---> it is a standalone value {it have a value}--(its type is --> object)
+ // undefined==> it means that abhi value assign nahi hue hai 
+ // symbol==>mostly used for Unique 
+
+ // object==> 
+ console.log(typeof undefined);  // undefined
+ console.log(typeof  null);   // object 
+
+
